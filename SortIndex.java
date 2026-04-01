df4293b8f3962cebbd8b9bc5f62fc1eeb0497457
ggpark9703 <81746385+ggpark9703@users.noreply.github.com>
2026-04-01 20:49:51 +0900

    Create SortIndex.java

---
+++ SortIndex.java
@@ -0,0 +1,30 @@
+/*
+문제:
+1 이상 1,000,000,000 이하의 서로 다른 정수들로 이루어진 배열 arr이 주어집니다.
+배열 arr을 오름차순으로 정렬했을 때 정렬하기 전의 arr에서의 
+인덱스를 순서대로 담은 배열을 return 하는 solution 함수를 작성해 주세요.
+
+해결방법:
+기존 주어진 문자열과 인덱스값을 저장하기 위해서 HashMap을 활용하면 좋겠다고 먼저 생각했다
+1. 해쉬맵에 기존 배열 인덱스 값 저장 
+2. 주어진 배열 .sort 메서드로 오름차순 정렬
+3. 오름차순으로 정렬된 결과 값을 순서대로 읽을 루프문 작성 
+3.1 해당 루프문에서 map이 가지고 있는 index값을 answer값으로 대체
+ 
+*/
+import java.util.HashMap;
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[arr.length];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],i);
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            answer[i]=map.get(arr[i]);
+        }
+        return answer;
+    }
+}
